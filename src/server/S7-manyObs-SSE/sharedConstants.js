0771b478d11c5a80fecc143df4f9214e5763d845
Dierk Koenig <dierk.koenig@canoo.com>
2025-04-10 22:39:24 +0200

    many remote observables

---
+++ src/server/S7-manyObs-SSE/sharedConstants.js
@@ -0,0 +1,7 @@
+
+export { channelName, updateActionName, updateActionParam, obsNameParam }
+
+const channelName       = "remoteObservable";
+const obsNameParam      = "name";
+const updateActionName  = "setValue";
+const updateActionParam = "value";
