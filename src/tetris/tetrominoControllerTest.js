7d8e77740470f6bce19251cc0a028d1ca452d68c
Dierk Koenig <dierk.koenig@canoo.com>
2025-05-10 16:31:32 +0200

    tests updated

--- src/tetris/tetrominoControllerTest.js
+++ src/tetris/tetrominoControllerTest.js
@@ -2,10 +2,8 @@
  * @module tetris/tetrominoControllerTest
  */
 import {TestSuite}                        from "../kolibri/util/test.js";
-import {Tetronimo}                        from "./shape.js";
-import {intersects, normalize, rotateYaw} from "./tetrominoController.js";
+import {normalize, rotateYaw} from "./tetrominoController.js";
 import {shapeEqual} from "./util.js";
-import {passive}    from "../server/S7-manyObs-SSE/remoteObservableMap.js";
 
 const controllerSuite = TestSuite("tetris/tetrominoControl");
 
@@ -33,30 +31,14 @@ controllerSuite.add("normalize", assert => {
     assert.isTrue( shapeEqual( normalize(tooLowShape0),     normalizedShape0));
 });
 
-controllerSuite.add("SShapeTetro collide", assert => {
-    const sTetro = /** @type { RemoteValueType<TetronimoType> } */ passive(Tetronimo(3,0));
-    assert.is(sTetro.value.shapeName, "shapeS");
-    sTetro.value.setPosition( {x:0,y:0,z:0} );
-    const spaceBoxes = [];
-    assert.is(intersects(sTetro, spaceBoxes), false);  // there is nothing to collide with
-    spaceBoxes.push(...(sTetro.value.boxes));
-    assert.is(intersects(sTetro, spaceBoxes), true);   // we collide with our own positions
-    const secondSTetro = /** @type { RemoteValueType<TetronimoType> } */ passive(Tetronimo(3,1));
-    secondSTetro.value.setPosition( {x:1,y:1,z:0} );          // this should now snugly fit
-    assert.is(intersects(secondSTetro, spaceBoxes), false);
-});
+controllerSuite.add("rotateYaw", assert => {
 
-controllerSuite.add("rotateYaw (incl roll/pitch)", assert => {
-    const sTetro = /** @type { RemoteValueType<TetronimoType> } */ passive(Tetronimo(3,0));
-    assert.is(sTetro.value.shapeName, "shapeS");
-    sTetro.value.setPosition( {x:0,y:0,z:0} );
     const shapeS = [
         {x: 2, y: 0, z: 0},
         {x: 1, y: 0, z: 0},
         {x: 1, y: 1, z: 0},
         {x: 0, y: 1, z: 0},
     ];
-    assert.isTrue( shapeEqual( sTetro.value.getShape(), shapeS ));
 
     const rotatedShapeS = [
         {x: 0, y: 0, z: 0},
