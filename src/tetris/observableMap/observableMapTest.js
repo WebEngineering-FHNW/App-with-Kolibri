acc2fada99eeb3fdb71bca292199fd55967c65a1
Dierk Koenig <dierk.koenig@canoo.com>
2025-05-10 17:28:28 +0200

    clarify the observableMapTest.js

--- src/tetris/observableMap/observableMapTest.js
+++ src/tetris/observableMap/observableMapTest.js
@@ -1,25 +1,39 @@
 import {TestSuite}     from "../../kolibri/util/test.js";
-import {ObservableMap} from "./observableMap.js";
-import {POISON_PILL}   from "../../server/S7-manyObs-SSE/remoteObservableMap.js";
+import {ObservableMap}       from "./observableMap.js";
+import {active, POISON_PILL} from "../../server/S7-manyObs-SSE/remoteObservableMap.js";
 
 const suite = TestSuite("observable/observableMap");
 
 suite.add("basic", assert => {
 
-    let observed          = null;
-    const newNameCallback = v => observed = v;
-    const map             = ObservableMap(newNameCallback);
-    assert.is(observed, null); // no callback on init
+    let exampleObs     = null;
+    let observedValue = null;
+    const onNewObservableNameCallback = namedObs => {
+        if(namedObs.id === "example") {
+            exampleObs = namedObs.observable;
+            exampleObs.onChange( it => observedValue = it);
+        }
+    };
+    const observableMap = ObservableMap(onNewObservableNameCallback);
+    assert.is(exampleObs, null); // of course
 
-    map.addObservableForID("a");
-    assert.is(observed.id, "a"); // now we got the callback
-    assert.is(observed.observable.getValue().mode, "passive");  // with mode and value
-    assert.is(observed.observable.getValue().value, undefined); // (initially undefined)
+    observableMap.addObservableForID("example");
 
-    map.removeObservableForID("a");
-    assert.is(observed.id, "a"); // now we got the callback
-    assert.is(observed.observable.getValue(), POISON_PILL);
+    assert.isTrue(!! exampleObs);
 
+    // observable was created with an initial value of undefined
+
+    assert.is(observedValue.mode, "passive");  // with mode and value
+    assert.is(observedValue.value, undefined); // initially undefined
+
+    exampleObs.setValue(active("firstValue"));
+
+    assert.is(observedValue.mode,  "active");
+    assert.is(observedValue.value, "firstValue");
+
+    observableMap.removeObservableForID("example");
+    assert.is(observedValue, POISON_PILL);
 });
+
 
 suite.run();
