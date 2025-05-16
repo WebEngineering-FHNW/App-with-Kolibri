c61bc11f05fadf00227beabbba799be28cddfca3
Dierk Koenig <dierk.koenig@canoo.com>
2025-05-16 12:39:21 +0200

    om impl and 36 tests

--- src/tetris/observableMap/om.js
+++ src/tetris/observableMap/om.js
@@ -3,8 +3,11 @@
 /** @typedef { (key:String) => void}                    keyRemovedCallback  */
 /** @typedef { <_T_> (key:String, value:_T_) => void}   onChangeCallback - value is never undefined */
 
+import {Just, Nothing} from "../../kolibri/stdlib.js";
+export {OM}
+
 /**
- * @typedef ObservableMap
+ * @typedef OMType
  * @template _T_
  * @property { (key:String, value:_T_) => void}     setValue - implicit addKey
  * @property { (key:String) => MaybeType<_T_>}      getValue
@@ -16,3 +19,64 @@
 
 // ROM  = ObservableMap< RV <T> >
 // getMode: (function injected) __mode__
+
+/**
+ * @return { OMType }
+ * @constructor
+ */
+const OM = () => {
+
+    const backingMap      = {};
+    const addListeners    = [];
+    const removeListeners = [];
+    const changeListeners = [];
+
+    const onKeyAdded   = listener => addListeners   .push(listener);
+    const onKeyRemoved = listener => removeListeners.push(listener);
+    const onChange     = listener => {
+        changeListeners.push(listener);
+        for(const [key, value] of Object.entries(backingMap)){ // immediate callback
+            listener(key, value); // oldValue? removeMe?
+        }
+    };
+
+    const hasKey = key => backingMap.hasOwnProperty(key);
+
+    const setKeyValue = (key, value) => {
+        const keyIsNew   = !hasKey(key);
+        const valueIsNew = keyIsNew
+            ? true
+            : backingMap[key] !== value;
+
+        backingMap[key] = value;
+
+        if (keyIsNew) {
+            addListeners.forEach( callback => callback(key));
+        }
+        if(valueIsNew) {
+            changeListeners.forEach( callback => callback(key, value));
+        }
+    };
+
+    const removeKey = key => {
+        delete backingMap[key];
+        removeListeners.forEach( callback => callback(key));
+    };
+
+    const getValue = key =>
+        hasKey(key)
+        ? Just(backingMap[key])
+        : Nothing;
+
+    const setValue = (key, value) => {
+        if (undefined === value || null === value ) {
+            removeKey(key);
+        } else {
+            setKeyValue(key, value);
+        }
+    };
+
+    return {
+        getValue, setValue, removeKey, onKeyAdded, onKeyRemoved, onChange
+    }
+};
--- src/tetris/utilTest.js
+++ src/tetris/utilTest.js
@@ -1,4 +1,4 @@
-import {ownPropEqual, shapeEqual, str} from "./util.js";
+import {ownPropEqual, shapeEqual} from "./util.js";
 import {TestSuite}                     from "../kolibri/util/test.js";
 import "../kolibri/util/array.js"
 
@@ -16,39 +16,6 @@ utilSuite.add( "shapeEqual", assert => {
     assert.isTrue(shapeEqual(3..times(), 3..times()));
 }) ;
 
-utilSuite.add("str", assert => {
-    // Test primitive values
-    assert.is(str(null), 'null');
-    assert.is(str(undefined), 'undefined');
-    assert.is(str(42), '42');
-    assert.is(str('hello'), 'hello');
-    assert.is(str(true), 'true');
 
-    // Test arrays
-    assert.is(str([1, 2, 3]), '[1, 2, 3]');
-
-    // Test simple objects
-    const simpleObj = { a: 1, b: 'test' };
-    const simpleObjStr = str(simpleObj);
-    assert.isTrue(simpleObjStr.includes('a: 1'));
-    assert.isTrue(simpleObjStr.includes('b: test'));
-
-    // Test nested objects
-    const nestedObj = { 
-        name: 'test', 
-        details: { 
-            id: 123, 
-            active: true 
-        },
-        items: [1, 2, { label: 'item3' }]
-    };
-    const nestedObjStr = str(nestedObj);
-    assert.isTrue(nestedObjStr.includes('name: test'));
-    assert.isTrue(nestedObjStr.includes('details:'));
-    assert.isTrue(nestedObjStr.includes('id: 123'));
-    assert.isTrue(nestedObjStr.includes('active: true'));
-    assert.isTrue(nestedObjStr.includes('items:'));
-    assert.isTrue(nestedObjStr.includes('label: item3'));
-}) ;
 
 utilSuite.run();
