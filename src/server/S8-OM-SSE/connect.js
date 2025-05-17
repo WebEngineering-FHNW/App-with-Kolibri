9fad20b999ffbffa6f243203874e3043536e822c
Dierk Koenig <dierk.koenig@canoo.com>
2025-05-18 00:31:36 +0200

    using version number as hysterese guard

--- src/server/S8-OM-SSE/connect.js
+++ src/server/S8-OM-SSE/connect.js
@@ -1,12 +1,15 @@
-import {LoggerFactory}       from "../../kolibri/logger/loggerFactory.js";
-import {client}              from "../../kolibri/rest/restClient.js";
+
 import {
-    ACTION_KEY, KEY_PARAM,
+    ACTION_KEY, DATA_KEY, KEY_PARAM,
     PAYLOAD_KEY,
     REMOVE_ACTION_NAME,
     TOPIC_REMOTE_OBSERVABLE,
-    UPDATE_ACTION_NAME, UPDATE_ACTION_PARAM
-} from "./romConstants.js";
+    UPDATE_ACTION_NAME, UPDATE_ACTION_PARAM, VERSION_KEY
+}                      from "./romConstants.js";
+import {LoggerFactory} from "../../kolibri/logger/loggerFactory.js";
+import {client}        from "../../kolibri/rest/restClient.js";
+import {OM}            from "../../tetris/observableMap/om.js";
+import {AsyncRelay}    from "../../tetris/observableMap/asyncRelay.js";
 
 export { connect }
 
@@ -20,6 +23,13 @@ const log = LoggerFactory("ch.fhnw.kolibri.remote.connect");
  * @param { OMType } om      - the observable map that connects to the server
  */
 const connect = (baseUrl, om) => {
+
+    const rom = OM("remote");
+
+    const versions = {}; // keys to their latest known version number
+
+    const scheduler = AsyncRelay(rom)(om);
+
     const eventSource = new EventSource(baseUrl + '/' + TOPIC_REMOTE_OBSERVABLE);
 
     eventSource.addEventListener('message', event =>
@@ -34,28 +44,45 @@ const connect = (baseUrl, om) => {
         const payload = data[PAYLOAD_KEY];
         switch (action) {
             case UPDATE_ACTION_NAME:
-                om.setValue(payload.key, payload.value);
+                const key   = payload[KEY_PARAM];
+                const value = payload[UPDATE_ACTION_PARAM];
+                const receivedVersion = value[VERSION_KEY];
+                if (receivedVersion <= versions[key]) {
+                    log.debug(`ignore update: new version <= old version:  ${receivedVersion} <= ${versions[key]}.`);
+                    return;
+                }
+                versions[key] = receivedVersion; // set the highest version as soon as possible
+                scheduler.addOk( _=> {
+                    rom.setValue(key, value[DATA_KEY]);
+                });
                 break;
             case REMOVE_ACTION_NAME:
-                om.removeKey(payload.key);
+                scheduler.addOk( _=> {
+                    const key = payload[KEY_PARAM];
+                    rom.removeKey(key);
+                    delete versions[key]; // remove the version guard as late as possible
+                });
                 break;
             default:
                 log.error(`cannot process received data: ${event.data}`)
         }
     });
 
-    om.onChange( ( key, value ) => { // also handles the keyAdded case implicitly
+    rom.onChange( ( key, value ) => { // also handles the keyAdded case implicitly
+        const version = versions[key] ?? 0;
+        versions[key] = version + 1;
         const data = {
+            [VERSION_KEY]:          versions[key],
             [KEY_PARAM] :           key,
             [UPDATE_ACTION_PARAM] : value
         };
-        log.debug(`sending update key ${key} value ${value}`);
+        log.debug(`sending update key ${key} value ${value} version ${versions[key]}`);
         client(baseUrl + '/' + UPDATE_ACTION_NAME, "POST", data)
             .then(  _ => log.debug("done sending update"))
             .catch( e => log.error("error sending update data: "+ JSON.stringify(data) + " " + e));
     } );
 
-    om.onKeyRemoved( ( key ) => {
+    rom.onKeyRemoved( ( key ) => {
         const data = {
             [KEY_PARAM] :           key
         };
