23f9d2aea1eb2657bd58f688521206af51da5c80
Dierk Koenig <dierk.koenig@canoo.com>
2025-05-17 17:30:17 +0200

    ah - that works so nicely!

---
+++ src/server/S8-OM-SSE/romConstants.js
@@ -0,0 +1,29 @@
+/**
+ * @module remote/remoteObservableConstants
+ * Values that must be the same on client and server.
+ */
+
+/**
+ * Url path entry for registering the SSE channel that provides the service of remote observables.
+ * We currently use only one topic since the number of parallel SSE channels from the same
+ * client is limited (6 with http/1, 100 with http/2).
+ */
+export const TOPIC_REMOTE_OBSERVABLE = "remoteObservable";
+
+/** request parameter that contains the id of the observable */
+export const OBSERVABLE_ID_PARAM = "key";
+
+/** url path entry for the action that sets the value of a remote observable */
+export const UPDATE_ACTION_NAME  = "setValue";
+/** request and response parameter that contains the value for the update */
+export const UPDATE_ACTION_PARAM = "value";
+
+/** url path entry for the action that reads the value of a remote observable */
+export const READ_ACTION_NAME    = "getValue";
+/** response parameter that contains the value being read */
+export const READ_ACTION_PARAM   = "value";
+
+/** url path entry for the action that removes the remote observable with
+ * key {@link OBSERVABLE_ID_PARAM} from the map of stored key/values */
+export const REMOVE_ACTION_NAME    = "remove";
+
