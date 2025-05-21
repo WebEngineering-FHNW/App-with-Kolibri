053498c38d64777ef1c45da165990f5335cd0e56
Dierk Koenig <dierk.koenig@canoo.com>
2025-05-22 00:01:41 +0200

    collision detection and fallDown activated

--- src/tetris/relationalModel.js
+++ src/tetris/relationalModel.js
@@ -4,10 +4,10 @@
  * the local game and the collaborative game.
  */
 
-import {MISSING_FOREIGN_KEY} from "../server/S7-manyObs-SSE/remoteObservableMap.js";
 import {shapesByName}        from "./shape.js";
+import {MISSING_FOREIGN_KEY} from "../extension/relationalModelType.js";
 
-export { Player, NO_PLAYER, Tetromino, NO_TETROMINO, Box, NO_BOX }
+export { Player, NO_PLAYER, Tetromino, NO_TETROMINO, Box, NO_BOX, GameState, NO_GAME_STATE }
 
 /**
  * @typedef {
@@ -83,12 +83,6 @@ const Box = paramObj => paramObj; // for the type safety
  */
 const NO_BOX = Box({id:MISSING_FOREIGN_KEY, tetroId: MISSING_FOREIGN_KEY, xPos:0, yPos:0, zPos:0});
 
-/**
- * Game state. The active user has to care for updating this.
- * @typedef GameStateModelType
- * @property { Boolean } fallingDown - Whether the current tetronimo is supposed to be falling.
- * @property { Number  } score
- */
 
 /**
  *
@@ -127,9 +121,28 @@ const NO_PLAYER = Player(MISSING_FOREIGN_KEY, "no name");
 
 
 /**
- * Array of all IDs of all known players in the game
- * @typedef { Array<ForeignKeyType> } AllPlayerIdsType
+ * Game state. The active user has to care for updating this.
+ * @typedef GameStateModelType
+ * @property { ForeignKeyType } id
+ * @property { Boolean } fallingDown - Whether the current tetronimo is supposed to be falling.
+ * @property { Number  } score
  */
+
+/**
+ * @param { ForeignKeyType } id
+ * @param { Boolean } fallingDown
+ * @param { Number } score
+ * @constructor
+ * @return {GameStateModelType}
+ */
+const GameState = (id, fallingDown, score) => ({id, fallingDown, score}); // for the type safety
+
+/**
+ * Null-Object Pattern
+ * @type { GameStateModelType }
+ */
+const NO_GAME_STATE = GameState(MISSING_FOREIGN_KEY, false, 0);
+
 
 // todo: it might need more types for game state (running, ended, etc. plus score, etc.)
 // todo: it might need "upcoming" tetromino
--- start.md
+++ start.md
@@ -2,3 +2,4 @@ cd src
 node server/S8-OM-SSE/romServer.js 
 
 open browser at http://86.119.43.169:8080/server/S8-OM-SSE/index.html
+open browser at http://86.119.43.169:8080/tetris/index.html
