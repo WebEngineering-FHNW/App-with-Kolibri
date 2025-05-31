5831f63021893d97ef86bbde0a9428cb0373cb72
Dierk Koenig <dierk.koenig@canoo.com>
2025-05-31 18:14:29 +0200

    setting the debounce timeouts such that they allow for reasonable responsiveness while still giving enough leeway for processing/display

--- src/tetris/startRemote.js
+++ src/tetris/startRemote.js
@@ -5,9 +5,9 @@ import {connect}               from "../server/S8-OM-SSE/connect.js";
 import {OM}                  from "./observableMap/om.js";
 import {LOG_DEBUG, LOG_WARN} from "../kolibri/logger/logLevel.js";
 
-defaultConsoleLogging("ch.fhnw.tetris", LOG_DEBUG);
+defaultConsoleLogging("ch.fhnw.tetris", LOG_INFO);
 
-const om = OM("index.html", 10); // 1 in minimum to make updates async
+const om = OM("index.html", 20); // 1 in minimum to make updates async
 
 connect(window.location.origin, om);
 
