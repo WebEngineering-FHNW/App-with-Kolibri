c0e373792f266261247ac3ff695bc4ae6eb74b41
Dierk Koenig <dierk.koenig@canoo.com>
2025-05-31 14:25:32 +0200

    renamings for consistency

--- src/server/S8-OM-SSE/connect.js
+++ src/server/S8-OM-SSE/connect.js
@@ -1,10 +1,10 @@
 
 import {
-    ACTION_KEY, DATA_KEY, KEY_PARAM,
-    PAYLOAD_KEY,
-    REMOVE_ACTION_NAME,
-    TOPIC_REMOTE_OBSERVABLE,
-    UPDATE_ACTION_NAME, UPDATE_ACTION_PARAM, VERSION_KEY
+    KEY_ACTION, KEY_DATA, PARAM_KEY,
+    KEY_PAYLOAD,
+    PATH_REMOVE_ACTION,
+    PATH_REMOTE_OBSERVABLE,
+    PATH_UPDATE_ACTION, PARAM_UPDATE_ACTION, KEY_VERSION
 }                      from "./romConstants.js";
 import {LoggerFactory} from "../../kolibri/logger/loggerFactory.js";
 import {client}        from "../../kolibri/rest/restClient.js";
@@ -30,7 +30,7 @@ const connect = (baseUrl, om) => {
 
     const scheduler = AsyncRelay(rom)(om);
 
-    const eventSource = new EventSource(baseUrl + '/' + TOPIC_REMOTE_OBSERVABLE);
+    const eventSource = new EventSource(baseUrl + '/' + PATH_REMOTE_OBSERVABLE);
 
     eventSource.addEventListener('message', event =>
         log.warn("unknown event type " + event)
@@ -38,15 +38,15 @@ const connect = (baseUrl, om) => {
     eventSource.addEventListener('error', err =>
         log.error("SSE error " + err)
     );
-    eventSource.addEventListener(TOPIC_REMOTE_OBSERVABLE, event => {
+    eventSource.addEventListener(PATH_REMOTE_OBSERVABLE, event => {
         const data    = JSON.parse(event.data);
-        const action  = data[ACTION_KEY];
-        const payload = data[PAYLOAD_KEY];
+        const action  = data[KEY_ACTION];
+        const payload = data[KEY_PAYLOAD];
         switch (action) {
-            case UPDATE_ACTION_NAME:
-                const key   = payload[KEY_PARAM];
-                const value = payload[UPDATE_ACTION_PARAM];
-                const receivedVersion = value[VERSION_KEY];
+            case PATH_UPDATE_ACTION:
+                const key   = payload[PARAM_KEY];
+                const value = payload[PARAM_UPDATE_ACTION];
+                const receivedVersion = value[KEY_VERSION];
                 if (receivedVersion < versions[key]) {
                     log.debug(`ignore update: new version < old version:  ${receivedVersion} <= ${versions[key]}.`);
                     return;
@@ -55,13 +55,13 @@ const connect = (baseUrl, om) => {
                     log.debug("version is equal but value might still have changed (?)");
                 }
                 scheduler.addOk( _=> {
-                    rom.setValue(key, value[DATA_KEY]);
+                    rom.setValue(key, value[KEY_DATA]);
                     versions[key] = receivedVersion; // this line must be exactly here or data will get missing
                 });
                 break;
-            case REMOVE_ACTION_NAME:
+            case PATH_REMOVE_ACTION:
                 scheduler.addOk( _=> {
-                    const key = payload[KEY_PARAM];
+                    const key = payload[PARAM_KEY];
                     rom.removeKey(key);
                     delete versions[key]; // remove the version guard as late as possible
                 });
@@ -75,22 +75,22 @@ const connect = (baseUrl, om) => {
         const version = versions[key] ?? 0;
         versions[key] = version + 1;
         const data = {
-            [VERSION_KEY]:          versions[key],
-            [KEY_PARAM] :           key,
-            [UPDATE_ACTION_PARAM] : value
+            [KEY_VERSION]:          versions[key],
+            [PARAM_KEY] :           key,
+            [PARAM_UPDATE_ACTION] : value
         };
         log.debug(`sending update key ${key} value ${value} version ${versions[key]}`);
-        client(baseUrl + '/' + UPDATE_ACTION_NAME, "POST", data)
+        client(baseUrl + '/' + PATH_UPDATE_ACTION, "POST", data)
             .then(  _ => log.debug("done sending update"))
             .catch( e => log.error("error sending update data: "+ JSON.stringify(data) + " " + e));
     } );
 
     rom.onKeyRemoved( ( key ) => {
         const data = {
-            [KEY_PARAM] :           key
+            [PARAM_KEY] : key
         };
         log.debug(`sending remove key ${key}`);
-        client(baseUrl + '/' + REMOVE_ACTION_NAME, "DELETE", data)
+        client(baseUrl + '/' + PATH_REMOVE_ACTION, "DELETE", data)
             .then(  _ => log.debug("done sending remove"))
             .catch( e => log.error("error sending remove data: "+ JSON.stringify(data) + " " + e));
     } );
--- src/server/S8-OM-SSE/romConstants.js
+++ src/server/S8-OM-SSE/romConstants.js
@@ -8,22 +8,24 @@
  * We currently use only one topic since the number of parallel SSE channels from the same
  * client is limited (6 with http/1, 100 with http/2).
  */
-export const TOPIC_REMOTE_OBSERVABLE = "remoteObservable";
-
-/** request parameter that contains the id of the observable */
-export const KEY_PARAM   = "key";
-
-export const ACTION_KEY  = "action";
-export const PAYLOAD_KEY = "payload";
-export const DATA_KEY    = "data";
-export const VERSION_KEY = "version";
+export const PATH_REMOTE_OBSERVABLE = "remoteObservable";
 
 /** url path entry for the action that sets the value of a remote observable */
-export const UPDATE_ACTION_NAME  = "setValue";
-/** request and response parameter that contains the value for the update */
-export const UPDATE_ACTION_PARAM = "value";
-
+export const PATH_UPDATE_ACTION = "setValue";
 /** url path entry for the action that removes the remote observable with
- * key {@link KEY_PARAM} from the map of stored key/values */
-export const REMOVE_ACTION_NAME    = "remove";
+ * key {@link PARAM_KEY} from the map of stored key/values */
+export const PATH_REMOVE_ACTION = "remove";
 
+/** request parameter that contains the id of the observable */
+export const PARAM_KEY = "key";
+/** request and response parameter that contains the value for an update */
+export const PARAM_UPDATE_ACTION = "value";
+
+/*
+ * Keys in the transferred object (DTO) that client and server have to agree upon
+ */
+
+export const KEY_ACTION  = "action";
+export const KEY_PAYLOAD = "payload";
+export const KEY_DATA    = "data";
+export const KEY_VERSION = "version";
